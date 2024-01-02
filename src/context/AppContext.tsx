22ce059e7fbb341b58bb49bc48ae6536976204a6
Esto Triramdani N <estolagi@gmail.com>
2024-01-02 20:08:22 +0700

    update: setup data fetching for addresses

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,19 +1,97 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { fetcher } from '@/lib/services';
-import { IProvince } from '@/types';
+import { IDistrict, IProvince, IRegency, IVillage } from '@/types';
 import React, { createContext, useState } from 'react';
 import useSWR from 'swr';
 
-const AppContext = createContext({});
+interface IAppContext {
+  province: IProvince | null;
+  regency: IRegency | null;
+  district: IDistrict | null;
+  village: IVillage | null;
+  handleChangeAddress: (type: string, value: any) => void;
+  provinces?: IProvince[];
+  regencies?: IRegency[];
+  districts?: IDistrict[];
+  villages?: IVillage[];
+}
+
+const AppContext = createContext<IAppContext>({
+  district: null,
+  province: null,
+  regency: null,
+  village: null,
+  handleChangeAddress: () => null,
+});
 
 export default AppContext;
 
 export const AppContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [provinceData, setProvinceData] = useState<IProvince>();
+  const [province, setProvince] = useState<IProvince | null>(null);
+  const [regency, setRegency] = useState<IRegency | null>(null);
+  const [district, setDistrict] = useState<IDistrict | null>(null);
+  const [village, setVillage] = useState<IVillage | null>(null);
 
   const { data: provinces } = useSWR<IProvince[]>(
     `${import.meta.env.VITE_APP_WILAYAH_API_URL}/provinces.json`,
     fetcher,
   );
 
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const { data: regencies } = useSWR<IRegency[]>(
+    `${import.meta.env.VITE_APP_WILAYAH_API_URL}/regencies/${province?.id}.json`,
+    province?.id ? fetcher : null,
+  );
+
+  const { data: districts } = useSWR<IDistrict[]>(
+    `${import.meta.env.VITE_APP_WILAYAH_API_URL}/districts/${regency?.id}.json`,
+    regency?.id ? fetcher : null,
+  );
+
+  const { data: villages } = useSWR<IVillage[]>(
+    `${import.meta.env.VITE_APP_WILAYAH_API_URL}/villages/${district?.id}.json`,
+    district?.id ? fetcher : null,
+  );
+
+  const handleChangeAddress = (type: string, value: any) => {
+    switch (type) {
+      case 'province':
+        setProvince(value);
+        setRegency(null);
+        setDistrict(null);
+        setVillage(null);
+        break;
+      case 'regency':
+        setRegency(value);
+        setDistrict(null);
+        setVillage(null);
+        break;
+      case 'district':
+        setDistrict(value);
+        setVillage(null);
+        break;
+      case 'village':
+        setVillage(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        province,
+        regency,
+        district,
+        village,
+        handleChangeAddress,
+        provinces,
+        regencies,
+        districts,
+        villages,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,21 @@ export interface IProvince {
   id: string;
   name: string;
 }
+
+export interface IRegency {
+	id: string;
+	province_id: string;
+	name: string;
+}
+
+export interface IDistrict {
+	id: string;
+	regency_id: string;
+	name: string;
+}
+
+export interface IVillage {
+	id: string;
+	district_id: string;
+	name: string;
+}
