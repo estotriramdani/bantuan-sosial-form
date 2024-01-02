ae51b36e08695d8fdcc2549a1eb42767b18ba279
Esto Triramdani N <estolagi@gmail.com>
2024-01-02 13:30:41 +0700

    update: add `success` variants to toast component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { ThemeProvider } from './components/theme-provider';
-import AddFormPage from './pages/AddForm';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
+import AddFormPage from '@/pages/AddForm';
 
 export default function Home() {
   return (
     <ThemeProvider>
       <AddFormPage />
+      <Toaster />
     </ThemeProvider>
   );
 }
