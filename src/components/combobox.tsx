1171dce0094f11ae85c96cfbb2a1861c5c5f90df
Esto Triramdani N <estolagi@gmail.com>
2024-01-03 14:12:52 +0700

    update: allow custom value combobox

--- src/components/combobox.tsx
+++ src/components/combobox.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ChevronsUpDown, Check } from 'lucide-react';
+import { ChevronsUpDown, Check, Plus } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '@/components/ui/command';
@@ -13,23 +13,45 @@ interface Props {
   setValue: (value: string) => void;
   data: { value: string; label: string }[];
   onSelect?: (value: string) => void;
+  allowCustomValue?: boolean;
 }
 
-export const ComboBox: React.FC<Props> = ({ onSelect, setValue, value, data }) => {
+export const ComboBox: React.FC<Props> = ({ onSelect, setValue, value, data, allowCustomValue }) => {
   const [open, setOpen] = useState(false);
+  const [tempValue, setTempValue] = useState('');
+
+  const foundSelected = data.find((item) => item.value?.toLowerCase() === value?.toLowerCase());
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button variant="outline" role="combobox" aria-expanded={open} className="justify-between w-full">
-          {value ? data.find((item) => item.value?.toLowerCase() === value?.toLowerCase())?.label : 'Cari...'}
+          {value && allowCustomValue && !foundSelected && value}
+          {value && foundSelected && foundSelected?.label}
+          {!value && 'Pilih...'}
           <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="p-0">
         <Command className="max-h-[200px] overflow-y-auto">
-          <CommandInput onValueChange={(e) => console.log(e)} placeholder="Cari..." />
-          <CommandEmpty>Tidak ditemukan.</CommandEmpty>
+          <CommandInput onValueChange={(e) => setTempValue(e)} placeholder="Cari..." />
+          <CommandEmpty>
+            <strong>{tempValue}</strong>. Tidak ditemukan.
+            {allowCustomValue && (
+              <div className="mt-2">
+                <Button
+                  size="icon"
+                  title="Tambahkan"
+                  onClick={() => {
+                    setValue(tempValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Plus />
+                </Button>
+              </div>
+            )}
+          </CommandEmpty>
           <CommandGroup>
             {data.map((item) => (
               <CommandItem
